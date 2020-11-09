d75a33a501395a30ee7d2ce64a0642beb754a1e3
rizkyramadhana01 <s11820034@student.unklab.ac.id>
2020-11-09 20:55:50 +0700

    Exercise #7 (Variabel & Operator JavaScript)

---
+++ pertemuan_23/index.js
@@ -0,0 +1,84 @@
+// External JavaScript
+// console.log("Halo Semua!");
+
+//Mendeklarasikan Variabel
+/*
+let kata1 = "Halo";
+let kata2 = "Semua";
+*/
+
+//Penulisan nama variabel yang benar
+/*
+let nama;
+let tempatTinggal;
+let _nomorHandphone;
+let $nomorAkunBank;
+*/
+
+// Penulisan nama variabel yang salah
+/*
+let umur saya; 
+let @*#!%()-+user; tidak boleh diawali dengan angka atau simbol
+let delete;
+*/
+
+// Tipe Data JavaScript
+/*
+let nama = "Rizky"; dapat diubah nilainya
+const namaAyah = "Hadi"; tidak dapat diubah nilainya
+*/
+
+// Menampilkan Data di Console
+/*
+let nama = "Rizky";
+let umur = 20;
+console.log(nama);
+console.log(umur);
+*/
+
+// Operator Aritmatika & String Operator
+/*
+let atas = 5;
+let bawah = 5;
+const namaPertama = "Rizky";
+const namaKedua = "Ramadhana";
+console.log(atas + bawah);
+console.log(atas - bawah);
+console.log(atas * bawah);
+console.log(atas / bawah);
+console.log("Nama saya adalah " + namaPertama + " " + namaKedua);
+*/
+
+// Operator Perbandingan
+/*
+let x = 5;
+let y = 5;
+let i = "5";
+console.log(x == y); 
+console.log(y === i); 
+console.log(x != 6);
+console.log(x !== "5");
+*/
+
+// Ternary Operator
+/*
+let angka = 5;
+let hasil = angka <= 0 ? "Negatif" : "Positif";
+console.log(hasil);
+*/
+
+// Operator Logika
+/*
+let kondisiA = true;
+let kondisiB = true;
+console.log(kondisiA && kondisiB);
+console.log(kondisiA || kondisiB);
+console.log(!kondisiA);
+*/
+
+// Exercise #7
+
+let tinggi = prompt("Masukan tinggi : ");
+let berat = prompt("Masukan berat : ");
+let hasil = berat / (tinggi * tinggi);
+console.log(hasil);
